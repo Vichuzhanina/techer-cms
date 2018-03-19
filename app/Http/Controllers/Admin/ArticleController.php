e87160ca9f73b788cbb1a5044b69901078fb9893
Nastya Vichuzhanina <vichuzhaninanas19@mail.ru>
2018-03-19 19:31:17 +0300

    Admin: article create

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Article;
+use App\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -26,7 +27,11 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.articles.create', [
+           'article'    => [],
+           'categories' => Category::with('children')->where('parent_id', 0)->get(),
+           'delimiter'  => ''
+         ]);
     }
     /**
      * Store a newly created resource in storage.
@@ -36,7 +41,12 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $article = Article::create($request->all());
+        // Categories
+        if ($request->input('categories')) {
+            $article->categories()->attach($request->input('categories'));
+        }
+        return redirect()->route('admin.article.index');
     }
     /**
      * Display the specified resource.
