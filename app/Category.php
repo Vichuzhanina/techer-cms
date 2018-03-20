3e88f95e3c933eb7c7916e323a445e9c1ca2d5e6
Nastya Vichuzhanina <vichuzhaninanas19@mail.ru>
2018-03-20 13:21:36 +0300

    Admin dashboard: menu; article, category: list and count

--- app/Category.php
+++ app/Category.php
@@ -13,14 +13,26 @@ class Category extends Model
     // Mutators
     public function setSlugAttribute($value)
     {
-        $this->attributes['slug'] = Str::slug( mb_substr($this->title, 0, 40)
-                                  . "-"
-                                  . \Carbon\Carbon::now()->format('dmyHi'), '-');
+
+        $this->attributes['slug'] = Str::slug(mb_substr($this->title, 0, 40)
+                                    . "-"
+                                    . \Carbon\Carbon::now()->format('dmyHi'), '-');
     }
 
     // Get children category
     public function children()
     {
         return $this->hasMany(self::class, 'parent_id');
+    }
+
+    // Polymorphic relation with articles
+    public function articles()
+    {
+        return $this->morphedByMany('App\Article', 'categoryable');
+    }
+
+    public function scopeLastCategories($query, $count)
+    {
+        return $query->orderBy('created_at', 'desc')->take($count)->get();
     }
 }
